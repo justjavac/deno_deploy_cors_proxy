d2e44f8ae2b66ecad6a253b97318aee6f1ea5ece
迷渡 <justjavac@gmail.com>
2022-07-28 17:24:00 +0800

    add gfm
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
+import { CSS, render } from "https://deno.land/x/gfm@0.1.22/mod.ts";
 
 function addCorsIfNeeded(response: Response) {
   const headers = new Headers(response.headers);
@@ -39,10 +40,37 @@ async function handleRequest(request: Request) {
     return new Response(response.body, { ...response, headers });
   }
 
-  const usage = await Deno.readFile("README.md");
-  return new Response(usage, {
+  const readme = await Deno.readTextFile("./README.md");
+  const body = render(readme);
+  const html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GitHub Raw Proxy</title>
+        <style>
+          body {
+            margin: 0;
+            background-color: var(--color-canvas-default);
+            color: var(--color-fg-default);
+          }
+          main {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem 1rem;
+          }
+          ${CSS}
+        </style>
+      </head>
+      <body data-color-mode="auto" data-light-theme="light" data-dark-theme="dark">
+        <main class="markdown-body">
+          ${body}
+        </main>
+      </body>
+    </html>`;
+  return new Response(html, {
     headers: {
-      "content-type": "text/plain",
+      "content-type": "text/html;charset=utf-8",
     },
   });
 }
